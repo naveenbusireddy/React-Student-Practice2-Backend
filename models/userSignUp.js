9171cec0899e751f708db510a1cebc5be8b00e79
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-04-18 15:16:31 +0530

    Added new user login API and validating with existing users

--- models/userSignUp.js
+++ models/userSignUp.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-const passwordComplexity = require("joi-password-complexity");
-const Joi = require("joi");
-const jwt = require("jsonwebtoken");
+// const passwordComplexity = require("joi-password-complexity");
+// const Joi = require("joi");
+
 
 const userSignUpSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String},
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true},
   phoneNo: { type: Number },
 });
+
 
 module.exports = mongoose.model("userSignUp", userSignUpSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,82 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const { check, validationResult } = require("express-validator");
+const JWT = require("jsonwebtoken");
 
 const StudentPractice = require("../models/student");
+const userSignUp = require("../models/userSignUp");
 
-const users = require("../models/userSignUp");
+// Router for User Sign Up purpose
+router.post("/signUp", [ check("email", "Please provide Valid Email").isEmail(),
+check("password", "Password should be more than 8 characters").isLength({min:8}) ],
+  async (req, res) => {
 
-router.post("/userSignUp", async (req, res) => {
-    const data = new users({
+    try {
+      const errors  = validationResult(req)
+      if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+      }
+
+      let userEmail =await userSignUp.findOne({email: req.body.email});
+      if(userEmail)
+      {
+        return res.send(`User with given email already exist`)
+      }
+      
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const data = new userSignUp({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
-        phoneNo: req.body.phoneNo
-    });
+        password: hashedPassword,
+        phoneNo: req.body.phoneNo,
+      });
+      await data.save();
+      // res.status(200).json(dataToSave);
+      let email=req.body.email;
+      const token = await JWT.sign({email}, "bys65ld53nkr32lr28dr01akr29aw23", {expiresIn:3600000})
+      res.json({token})
+      res.send(`Registered Successfully`);
+    } 
+    catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+);
+
+//Authentication Route
+router.post("/login", async (req, res) => {
     
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave);
-        res.send(result);
+  try{
+    const loginUser = await userSignUp.findOne({email: req.body.email});
+
+    if(!loginUser)
+    {
+      return res.status(422).json({ message: "Invalid Credentials" });
     }
-    catch(error) {
-        res.status(500).json({message: error.message});
+    const isMatch = await bcrypt.compare(req.body.password, loginUser.password);
+    if(!isMatch)
+    {
+      return res.status(422).json({message: "Invalid Credentials"});
     }
+    
+    let email=loginUser.email;
+    const token = await JWT.sign({email}, "bys65ld53nkr32lr28dr01akr29aw23", {expiresIn:3600000})
+    res.json({token})
+    //res.send("Login Successful");
+  }
+  catch(error) {
+    res.status(500).send({message: "Internal Server error"});
+  }
 })
+
 //Post Method
 router.post("/saveDetails", async (req, res) => {
   console.log(req.body);
   // res.send('Post API');
-  if(req.body._id){
+  if (req.body._id) {
     const id = req.body._id;
     const updatedData = req.body;
     const options = { new: true };
@@ -38,15 +86,14 @@ router.post("/saveDetails", async (req, res) => {
       options
     );
     res.send(result);
-  }else{
+  } else {
     const data = new StudentPractice({
-        studentName: req.body.studentName,
-        university: req.body.university,
-      });
-      const dataToSave = await data.save();
-      res.status(200).json(dataToSave);
+      studentName: req.body.studentName,
+      university: req.body.university,
+    });
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
   }
- 
 });
 
 //Get All Methods
@@ -102,6 +149,5 @@ router.delete("/delete/:id", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
 
 module.exports = router;
